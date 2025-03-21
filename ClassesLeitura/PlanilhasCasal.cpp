e40e77cdce9a599bea518c7ac2b9bfacc8acc57a
heitorps <heitorps2020@gmail.com>
2025-03-21 14:36:00 -0300

    criada planilha casal

--- ClassesLeitura/PlanilhasCasal.cpp
+++ ClassesLeitura/PlanilhasCasal.cpp
@@ -34,19 +34,65 @@ namespace Leitura
                     int numero = stoi(t.next());
                     string complemento = t.next();
                     
-                    Casal* c = Controle::getCasalById(idPessoa1,idPessoa2);
-                    if (c == nullptr)
+                    PessoaFisica    *p1 = Controle::getPessoaFisica(idPessoa1),
+                                    *p2 = Controle::getPessoaFisica(idPessoa2);
+
+                    if(p1 != nullptr && p2 != nullptr){
+                        Casal* c = Controle::getCasal(idPessoa1,idPessoa2);
+                        if (c == nullptr)
+                        {
+                            c = new Casal(p1,p2);
+                            Controle::add(idPessoa1, c);
+                        }
+                        Lar *l = new Lar(rua,complemento,numero);
+                        Controle::add(idLar, l);
+                        c->setLar(l);
+                    }else
                     {
-                        //
-                    }else{
-                        //
-                    }
-                    
+                        //EXCEPTION PESSOA INEXISTENTE
+                    }                                                          
                 }
-                
             }
-            
         }
-        
+    }
+
+    void lePlanilhaCasamentos(const string &pasta, Controle*){
+        fstream arq;
+        arq.open(pasta + "casamentos.csv", ios::in);
+        string linha;
+        if (arq.is_open())
+        {
+            while (!arq.eof())
+            {
+                getline(arq, linha);
+                std::cout << linha << std::endl;
+                auto t = Tokenizer(linha, ';');
+                while (t.hasNext())
+                {
+                    string  idCasamento = t.next(), idPessoa1 = t.next(),
+                            idPessoa2 = t.next();
+                    time_t data = cpp_util::parseDate(t.next(), cpp_util::DATE_FORMAT_PT_BR_SHORT);
+                    string hora = t.next(), local = t.next();
+                    
+                    PessoaFisica    *p1 = Controle::getPessoaFisica(idPessoa1),
+                                    *p2 = Controle::getPessoaFisica(idPessoa2);
+
+                    if(p1 != nullptr && p2 != nullptr){
+                        Casal* c = Controle::getCasal(idPessoa1,idPessoa2);
+                        if (c == nullptr)
+                        {
+                            c = new Casal(p1,p2);
+                            Controle::add(idPessoa1, c);
+                        }
+                        Casamento *casamento = new Casamento(local, data, hora);
+                        Controle::add(idCasamento, casamento);
+                        c->setCasamento(casamento);
+                    }else
+                    {
+                        //EXCEPTION PESSOA INEXISTENTE
+                    }                   
+                }                
+            }            
+        }
     }
 }
