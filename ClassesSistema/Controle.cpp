d414c8006994fba5bfbc02554d45c5ae63668499
heitorps <heitorps2020@gmail.com>
2025-03-21 14:33:47 -0300

    controle com tabelas hash estáticas de todas as classes

--- ClassesSistema/Controle.cpp
+++ ClassesSistema/Controle.cpp
@@ -3,27 +3,46 @@
 //
 
 #include "Controle.h"
-
 #include "Loja.h"
 
 namespace Sistema {
-    void Controle::add(PessoaFisica *pf) {
-        pessoasFisicas.push_back(pf);
+    void Controle::add(string id, PessoaFisica *pf) {
+        pessoasFisicas.insert(pair<string,PessoaFisica*>(id,pf));
     }
 
-    void Controle::add(PessoaJuridica *pj) {
-        pessoasJuridicas.push_back(pj);
+    void Controle::add(string id, Casal *c){
+        casais.insert(pair<string,Casal*>(id,c));
     }
 
-    void Controle::add(Loja *lj) {
-        lojas.push_back(lj);
+    void Controle::add(string id, Casamento *c){
+        casamentos.insert(pair<string,Casamento*>(id,c));
     }
 
-    void Controle::add(Festa *fe) {
-        festas.push_back(fe);
+    void Controle::add(string id, Lar *l){
+        lares.insert(pair<string,Lar*>(id,l));
     }
 
-    Casal* Controle::getCasalById(string &id1, string &id2){ //tenta achar o casal no Map por cada um dos ids das pessoas
+    void Controle::add(string id, PessoaJuridica *pj) {
+        pessoasJuridicas.insert(pair<string,PessoaJuridica*>(id,pj));
+    }
+
+    void Controle::add(string id, Loja *lj) {
+        lojas.insert(pair<string,Loja*>(id,lj));
+    }
+
+    void Controle::add(string id, Festa *fe) {
+        festas.insert(pair<string,Festa*>(id,fe));
+    }
+
+    void Controle::add(string id, Tarefa *tf) {
+        tarefas.insert(pair<string,Tarefa*>(id,tf));
+    }
+
+    void Controle::add(string id, Compra *cp) {
+        compras.insert(pair<string,Compra*>(id,cp));
+    }
+    
+    Casal* Controle::getCasal(string id1, string id2){ //tenta achar o casal no Map por cada um dos ids das pessoas
         if (casais.count(id1) > 0)                          //vai que a entrada do lar/casamento tem os mesmos ids porém invertidos em ordem
         {
             return casais.at(id1);
@@ -34,11 +53,52 @@ namespace Sistema {
         else return nullptr;
     }
 
-    void Controle::add(Tarefa *tf) {
-        tarefas.push_back(tf);
+    PessoaFisica* Controle::getPessoaFisica(string id){
+        if (pessoasFisicas.count(id) > 0)
+        return pessoasFisicas.at(id);
+        else return nullptr;
     }
 
-    void Controle::add(Compra *cp) {
-        compras.push_back(cp);
+    PessoaJuridica* Controle::getPessoaJuridica(string id){
+        if (pessoasJuridicas.count(id) > 0)
+        return pessoasJuridicas.at(id);
+        else return nullptr;
+        
+    }
+    Loja* Controle::getLoja(string id){
+        if (lojas.count(id) > 0)
+        return lojas.at(id);
+        else return nullptr;
+        
+    }
+    Festa* Controle::getFesta(string id){
+        if (festas.count(id) > 0)
+        return festas.at(id);
+        else return nullptr;
+        
+    }
+    Casamento* Controle::getCasamento(string id){
+        if (casamentos.count(id) > 0)
+        return casamentos.at(id);
+        else return nullptr;
+        
+    }
+    Lar* Controle::getLar(string id){
+        if (lares.count(id) > 0)
+        return lares.at(id);
+        else return nullptr;
+        
+    }
+    Tarefa* Controle::getTarefa(string id){
+        if (tarefas.count(id) > 0)
+        return tarefas.at(id);
+        else return nullptr;
+        
+    }
+    Compra* Controle::getCompra(string id){
+        if (compras.count(id) > 0)
+        return compras.at(id);
+        else return nullptr;
+        
     }
 } // Sistema
--- ClassesSistema/Controle.h
+++ ClassesSistema/Controle.h
@@ -5,63 +5,79 @@
 #ifndef CONTROLE_H
 #define CONTROLE_H
 #include <list>
+#include <unordered_map>
 
 #include "Festa.h"
 #include "Loja.h"
 #include "PessoaFisica.h"
 #include "PessoaJuridica.h"
 #include "Casal.hpp"
+#include "Tarefa.h"
+#include "Casamento.hpp"
+#include "Lar.hpp"
 
 namespace Sistema {
 
 class Controle {
 private:
-    list<PessoaFisica*> pessoasFisicas;
-    list<PessoaJuridica*> pessoasJuridicas;
-    list<PessoaJuridica*> lojas;
-    list<Festa*> festas;
+    static unordered_map<string, PessoaFisica*> pessoasFisicas;
+    static unordered_map<string, PessoaJuridica*> pessoasJuridicas;
+    static unordered_map<string, Loja*> lojas;
+    static unordered_map<string, Festa*> festas;
     static unordered_map<string, Casal*> casais;
-    list<Tarefa*> tarefas;
-    list<Compra*> compras;
+    static unordered_map<string, Casamento*> casamentos;
+    static unordered_map<string, Lar*> lares;
+    static unordered_map<string, Tarefa*> tarefas;
+    static unordered_map<string, Compra*> compras;
 
 public:
     Controle() = default;
 
     ~Controle() = default;
 
-    [[nodiscard]] list<PessoaFisica *> pessoas_fisicas() const {
+    [[nodiscard]] unordered_map<string, PessoaFisica*> pessoas_fisicas() const {
         return pessoasFisicas;
     }
 
-    [[nodiscard]] list<PessoaJuridica *> pessoas_juridicas() const {
+    [[nodiscard]] unordered_map<string, PessoaJuridica *> pessoas_juridicas() const {
         return pessoasJuridicas;
     }
 
-    [[nodiscard]] list<PessoaJuridica *> lojas1() const {
+    [[nodiscard]] unordered_map<string, Loja *> lojas1() const {
         return lojas;
     }
 
-    [[nodiscard]] list<Festa *> festas1() const {
+    [[nodiscard]] unordered_map<string, Festa *> festas1() const {
         return festas;
     }
 
-    [[nodiscard]] std::list<Tarefa *> tarefas1() const {
+    [[nodiscard]] unordered_map<string, Tarefa *> tarefas1() const {
         return tarefas;
     }
 
-    [[nodiscard]] std::list<Compra *> compras1() const {
+    [[nodiscard]] unordered_map<string, Compra *> compras1() const {
         return compras;
     }
 
-    void add(PessoaFisica * pf);
-    void add(PessoaJuridica * pj);
-    void add(Loja *lj);
-    void add(Festa *fe);
-
-    static Casal* getCasalById(string &id1, string &id2);
-    void add(Tarefa * pf);
-    void add(Compra * pf);
-
+    static void add(string id, PessoaFisica * pf);
+    static void add(string id, Casal *c);
+    static void add(string id, Casamento *c);
+    static void add(string id, Lar *l);
+    static void add(string id, PessoaJuridica * pj);
+    static void add(string id, Loja *lj);
+    static void add(string id, Festa *fe);
+    static void add(string id, Tarefa * pf);
+    static void add(string id, Compra * pf);
+    
+    static Casal* getCasal(string id1, string id2);
+    static PessoaFisica* getPessoaFisica(string id);
+    static PessoaJuridica* getPessoaJuridica(string id);
+    static Loja* getLoja(string id);
+    static Festa* getFesta(string id);
+    static Casamento* getCasamento(string id);
+    static Lar* getLar(string id);
+    static Tarefa* getTarefa(string id);
+    static Compra* getCompra(string id);
 };
 
 } // Sistema
